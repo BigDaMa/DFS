bdfe8eb01dae21648af1ba0198464b3cf8701645
Felix Neutatz <neutatz@googlemail.com>
2019-03-18 10:23:34 +0100

    run ecoli

--- new_project/fastsklearnfeature/feature_selection/SimpleFeatureConstruction.py
+++ new_project/fastsklearnfeature/feature_selection/SimpleFeatureConstruction.py
@@ -381,16 +381,14 @@ if __name__ == '__main__':
     # dataset = ("/home/felix/datasets/ExploreKit/csv/phpn1jVwe_mammography.csv", 6)
 
 
-    dataset = (Config.get('iris.csv'), 4)
+    #dataset = (Config.get('iris.csv'), 4)
     #dataset = (Config.get('banknote.csv'), 4)
-    #dataset = (Config.get('ecoli.csv'), 8)
+    dataset = (Config.get('ecoli.csv'), 8)
     #dataset = (Config.get('abalone.csv'), 8)
     #dataset = (Config.get('breastcancer.csv'), 0)
     #dataset = (Config.get('transfusion.csv'), 4)
 
     selector = SimpleFeatureConstruction(dataset, c_max=9, save_logs=True)
-    #selector = ExploreKitSelection(dataset, KNeighborsClassifier(), {'n_neighbors': np.arange(3,10), 'weights': ['uniform','distance'], 'metric': ['minkowski','euclidean','manhattan']})
-
     selector.run()
 
 
