7eb702084a2fa9191d01ec89e48c7c70aa2d19b9
Felix Neutatz <neutatz@googlemail.com>
2019-05-13 08:28:26 +0200

    another

--- new_project/fastsklearnfeature/feature_selection/ComplexityDrivenFeatureConstruction.py
+++ new_project/fastsklearnfeature/feature_selection/ComplexityDrivenFeatureConstruction.py
@@ -663,8 +663,8 @@ if __name__ == '__main__':
 
 
     #paper featureset
-    selector = ComplexityDrivenFeatureConstruction(dataset, c_max=10, folds=10, max_seconds=None, save_logs=True, transformation_producer=get_transformation_for_cat_feature_space)
-    #selector = ComplexityDrivenFeatureConstruction(None, c_max=10, folds=10, max_seconds=None, save_logs=True, transformation_producer=get_transformation_for_cat_feature_space, reader=OnlineOpenMLReader(task_id, test_folds=4))
+    #selector = ComplexityDrivenFeatureConstruction(dataset, c_max=10, folds=10, max_seconds=None, save_logs=True, transformation_producer=get_transformation_for_cat_feature_space)
+    selector = ComplexityDrivenFeatureConstruction(None, c_max=10, folds=10, max_seconds=None, save_logs=True, transformation_producer=get_transformation_for_cat_feature_space, reader=OnlineOpenMLReader(task_id, test_folds=4))
 
     #selector = ComplexityDrivenFeatureConstruction(dataset, c_max=5, folds=10,
     #                                               max_seconds=None, save_logs=True, transformation_producer=get_transformation_for_cat_feature_space)
