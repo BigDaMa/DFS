1d63b77e7dc65c9188b39bc30c560001c5c3d930
Felix Neutatz <neutatz@googlemail.com>
2020-03-12 12:13:42 +0100

    clean up libraries

--- new_project/fastsklearnfeature/interactiveAutoML/new_bench/multiobjective/metalearning/openml_data/openml_meta_learning_random.py
+++ new_project/fastsklearnfeature/interactiveAutoML/new_bench/multiobjective/metalearning/openml_data/openml_meta_learning_random.py
@@ -1,110 +1,38 @@
-import autograd.numpy as anp
-import numpy as np
-from pymoo.util.misc import stack
-from pymoo.model.problem import Problem
-import numpy as np
-import pickle
-from fastsklearnfeature.candidates.CandidateFeature import CandidateFeature
-from fastsklearnfeature.candidates.RawFeature import RawFeature
-from fastsklearnfeature.interactiveAutoML.feature_selection.ForwardSequentialSelection import ForwardSequentialSelection
-from fastsklearnfeature.transformations.OneHotTransformation import OneHotTransformation
-from typing import List, Dict, Set
-from fastsklearnfeature.interactiveAutoML.CreditWrapper import run_pipeline
-from pymoo.algorithms.so_genetic_algorithm import GA
-from pymoo.factory import get_crossover, get_mutation, get_sampling
-from pymoo.optimize import minimize
-from pymoo.algorithms.nsga2 import NSGA2
-import matplotlib.pyplot as plt
-from fastsklearnfeature.interactiveAutoML.Runner import Runner
 import copy
-from sklearn.linear_model import LogisticRegression
-from fastsklearnfeature.candidates.CandidateFeature import CandidateFeature
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import make_scorer
-from sklearn.pipeline import Pipeline
 from sklearn.metrics import roc_auc_score
-from fastsklearnfeature.transformations.IdentityTransformation import IdentityTransformation
-from fastsklearnfeature.transformations.MinMaxScalingTransformation import MinMaxScalingTransformation
 from sklearn.linear_model import LogisticRegression
 import pickle
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedKFold
-import argparse
 import warnings
 warnings.filterwarnings("ignore")
 import pandas as pd
 import time
-
-from art.metrics import RobustnessVerificationTreeModelsCliqueMethod
-from art.metrics import loss_sensitivity
-from art.metrics import empirical_robustness
-from sklearn.pipeline import FeatureUnion
-
-
-from xgboost import XGBClassifier
 import numpy as np
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, GradientBoostingClassifier
 
-from art.classifiers import XGBoostClassifier, LightGBMClassifier, SklearnClassifier
-from art.attacks import HopSkipJump
-
-
-from fastsklearnfeature.interactiveAutoML.feature_selection.RunAllKBestSelection import RunAllKBestSelection
 from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import variance
 from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import model_score
-from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import fairness_score
-from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import robustness_score
 from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import chi2_score_wo
 from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import fcbf
 from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import my_mcfs
 from sklearn.model_selection import train_test_split
+from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import my_fisher_score
 
-from fastsklearnfeature.interactiveAutoML.new_bench import my_global_utils1
-from fastsklearnfeature.interactiveAutoML.new_bench.multiobjective.robust_measure import unit_test_score
-
-from sklearn.feature_selection import SelectKBest
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.svm import LinearSVC
-
-from sklearn.feature_selection import mutual_info_classif
-from sklearn.feature_selection import f_classif
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.compose import ColumnTransformer
-
-from fastsklearnfeature.interactiveAutoML.new_bench.multiobjective.robust_measure import robust_score_test
 
 from fastsklearnfeature.configuration.Config import Config
 
 
-from skrebate import ReliefF
-from fastsklearnfeature.interactiveAutoML.feature_selection.fcbf_package import my_fisher_score
-from sklearn.feature_selection import mutual_info_classif
-
-
 
 from functools import partial
-from hyperopt import fmin, hp, tpe, Trials, space_eval, STATUS_OK
+from hyperopt import fmin, hp, tpe, Trials, STATUS_OK
 
 from sklearn.feature_selection import mutual_info_classif
-from sklearn.feature_selection import f_classif
-
-
-from sklearn.model_selection import cross_val_score
 from fastsklearnfeature.interactiveAutoML.fair_measure import true_positive_rate_score
 from fastsklearnfeature.interactiveAutoML.new_bench.multiobjective.robust_measure import robust_score
 from skrebate import ReliefF
-
-from sklearn.ensemble import RandomForestRegressor
 import fastsklearnfeature.interactiveAutoML.new_bench.multiobjective.metalearning.strategies.multiprocessing_global as mp_global
-
-import sklearn
-
 import diffprivlib.models as models
 from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
 
 from fastsklearnfeature.interactiveAutoML.new_bench.multiobjective.metalearning.strategies.weighted_ranking import weighted_ranking
 from fastsklearnfeature.interactiveAutoML.new_bench.multiobjective.metalearning.strategies.hyperparameter_optimization import TPE
