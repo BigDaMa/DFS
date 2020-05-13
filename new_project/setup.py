5fb28079b28493d4c8ad8e2ede3261c804b9ca09
Felix Neutatz <neutatz@googlemail.com>
2020-05-13 14:09:11 +0200

    add hyperopt to setup

--- new_project/setup.py
+++ new_project/setup.py
@@ -33,7 +33,8 @@ setup(
                       "openml",
                       "diffprivlib",
                       "pebble",
-                      "arff2pandas"
+                      "arff2pandas",
+                      "hyperopt==0.2.3"
                       ],
     packages=find_packages(exclude=('tests', 'docs'))
 )
