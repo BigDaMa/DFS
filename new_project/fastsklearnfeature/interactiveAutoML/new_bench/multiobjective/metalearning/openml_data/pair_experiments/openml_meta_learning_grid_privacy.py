ef295c91f3dd87f945d5eaa3cd8a369bc10a3f69
Felix Neutatz <neutatz@googlemail.com>
2020-05-21 16:54:48 +0200

    change privacy range

--- new_project/fastsklearnfeature/interactiveAutoML/new_bench/multiobjective/metalearning/openml_data/pair_experiments/openml_meta_learning_grid_privacy.py
+++ new_project/fastsklearnfeature/interactiveAutoML/new_bench/multiobjective/metalearning/openml_data/pair_experiments/openml_meta_learning_grid_privacy.py
@@ -114,7 +114,7 @@ u_acc = 0.90
 
 results_heatmap = {}
 for min_accuracy in np.arange(l_acc, u_acc, (u_acc - l_acc) / 10.0):
-	for privacy in [0.7, 0.3, 0.1, 0.07, 0.03, 0.01]:
+	for privacy in [0.3, 0.1, 0.07, 0.03, 0.01, 0.007, 0.003, 0.001]:
 		i += 1
 
 		min_robustness = 0.0
