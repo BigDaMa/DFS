aa6e600e779cf640c02b419d4b3ee4ae8b884875
Felix Neutatz <neutatz@googlemail.com>
2020-07-25 07:27:19 +0200

    add distance

--- new_project/fastsklearnfeature/test/test_robustness/test_empirical_robustness_regresssion.py
+++ new_project/fastsklearnfeature/test/test_robustness/test_empirical_robustness_regresssion.py
@@ -5,6 +5,7 @@ from fastsklearnfeature.test.test_robustness.FGM_Regression import FastGradientM
 from fastsklearnfeature.test.test_robustness.LinearRegressionSKlearn import ScikitlearnLinearRegression
 from sklearn.metrics import r2_score
 import numpy.linalg as la
+import numpy as np
 
 diabetes = load_boston()
 
@@ -25,3 +26,7 @@ y_test_pred = model.predict(X_test)
 
 print('r2 score on original data: ' + str(r2_score(y_true=y_test, y_pred=y_test_pred)))
 print('r2 score on corrupted data: ' + str(r2_score(y_true=y_test, y_pred=y_test_attacked)))
+
+norm_type = -np.inf
+perts_norm = la.norm((X_test_attacked - X_test).reshape(X_test.shape[0], -1), ord=norm_type, axis=1)
+print('maximum absolute difference:' + str(np.sum(perts_norm)))
